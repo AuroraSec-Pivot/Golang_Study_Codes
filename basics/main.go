ebb156950ab62f1d2e3775c7ea50029473f46d6b
Aurora <guanhongwei0601@163.com>
2025-10-04 11:28:32 +0800

    main函数demo

---
+++ basics/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go Learning Playground 🚀")
+	fmt.Println("请选择要运行的示例：")
+	fmt.Println("1. Hello World")
+	fmt.Println("2. Goroutines")
+	// 这里可以用 flag 或 switch 来调度不同子目录的 demo
+}
